620fa42fe2b9880ff2562d26a9e009cabab009a1
Yu Chen Hou <houyuchen66@gmail.com>
2014-10-10 02:33:28 -0400

    Added working web server: currently cannot serve any files due to filesystem restrictions

--- background.js
+++ background.js
@@ -3,7 +3,34 @@ var Launcher = (function() {
   var l={};
 
   l.launchApp = function() {
+    //TODO make this less stupid: this manually sets the hosting folder each time
+    //var entry = "C:\Users\YuChen\Dropbox\Photos"
+    //window.entry = entry
+    //window.fs = new FileSystem(entry)
 
+    //Defines a handler to be used for the webserver
+    function MainHandler() {
+        BaseHandler.prototype.constructor.call(this)
+    }
+    _.extend(MainHandler.prototype, {
+        get: function() {
+            // handle get request
+            this.write('OK!, ' + this.request.uri)
+        }
+    })
+    for (var key in BaseHandler.prototype) {
+        MainHandler.prototype[key] = BaseHandler.prototype[key]
+    }
+
+    var handlers = [
+//        ['.*', MainHandler]
+//        ['.*', PackageFilesHandler]
+        ['.*', DirectoryEntryHandler]
+    ]
+    //Set up a web server with these settings and start it
+    var app = new chrome.WebApplication({handlers:handlers, port:8080})
+    app.start()
+    
     //If the app window is already open, bring it to the front
     if (l.windowId && chrome.app.window.get(l.windowId)) {
       chrome.app.window.get(l.windowId).focus();
@@ -34,5 +61,27 @@ var Launcher = (function() {
 
 }());
 
+chrome.app.runtime.onLaunched.addListener(function(launchData) {
+
+    var app = new chrome.WebApplication({handlers:handlers, port:8887})
+    app.start()
+});
 Launcher.init();
 
+//Some listeners for web server events
+chrome.runtime.onSuspend.addListener( function(evt) {
+    console.error('onSuspend',evt)
+    app.stop()
+})
+
+
+function reload() { chrome.runtime.reload() }
+chrome.runtime.onSuspendCanceled.addListener( function(evt) {
+    console.error('onSuspendCanceled',evt)
+})
+
+chrome.app.window.onClosed.addListener(function(evt) {
+    console.log('window closed. shutdown server, unload background page? hm?')
+})
+
+function reload() { chrome.runtime.reload() }
